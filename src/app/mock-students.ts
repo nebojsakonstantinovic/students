5715a82bcf52aec53ffeb79ab462aa1104079706
Frontend <frontend@Cubess-iMac-3.local>
2018-01-30 15:09:43 +0100

    adds students

---
+++ src/app/mock-students.ts
@@ -0,0 +1,15 @@
+import { Student } from './student';
+
+
+export const STUDENTS: Student[] = [
+  { id: 1, name: 'Milos Stoiljkovic' },
+  { id: 2, name: 'Nebojsa Konstantinovic' },
+  { id: 3, name: 'Dusan Ivanovic' },
+  { id: 4, name: 'Nikola Dosenovic' },
+  { id: 5, name: 'Borivoje Skrbina' },
+  { id: 6, name: 'Anka Markovic-Borak' },
+  { id: 7, name: 'Ivana Petrovic' },
+  { id: 8, name: 'Jovana Nikolic' },
+  { id: 9, name: 'Luka Cosic' },
+  { id: 10, name: 'Marija Jeremic' },
+];
---
+++ src/app/students/students.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student'
+import { STUDENTS } from '../mock-students';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  students = STUDENTS;
+
+  student: Student = {
+    id: 1;
+    name: 'Petar Petrovic'
+  };
+  // student = 'Petar Petrovic'
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelectedStudent() {
+    
+  }
+
+
+}
